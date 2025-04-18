b613dbd6a174b9992acb7dd2e708b74c30bbcc4a
ClaireF <claire.fernandes.ee@gmail.com>
2025-04-18 10:36:34 +0200

    fix(Producteur): fix collection producteur

--- models/Producteur.js
+++ models/Producteur.js
@@ -9,11 +9,27 @@ const ProducteurSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  mot_de_passe: {
+    type: String,
+    required: true
+  },
+  metier: {
+    type: String,
+    enum: ['agriculteur', 'éleveur', 'apiculteur', 'maraîcher', 'autre'],
+    required: true
+  },
   local_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Local',
     required: true
   }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Producteur', ProducteurSchema);
