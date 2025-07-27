88bbc59c2b8baa04fc421616e71f412829432637
Claire Fernandes <104427590+clairef2310@users.noreply.github.com>
2025-07-27 21:43:23 +0200

    Feat/add import routes (#10)
    * Fix/collection (#5)
    
    * feat(config): add base configuration
    
    * feat(models): add models
    
    * feat-controllers): add controllers
    
    * feat(routes): add routes
    
    * feat(models): add new models
    
    * fix(Producteur): fix collection producteur
    
    * fix(stock): correct name
    
    * Revert "Fix/collection (#5)" (#7)
    
    This reverts commit 415cd6612ed501befe4a4454e6d1a1be2e30acb6.
    
    * clean main (#11)
    
    * feat(config): add base configuration
    
    * feat(models): add models
    
    * feat-controllers): add controllers
    
    * feat(routes): add routes
    
    * feat(models): add new models
    
    * feat: add quality code
    
    * fix(producteur): fix producteur collection (#8)
    
    * clean main

---
+++ entity/producer.entity.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const producerSchema = new mongoose.Schema({
+  id: { type: String, unique: true, required: true },
+  geometry: {
+    type: { type: String, enum: ['Point'], required: true, default: 'Point' },
+    coordinates: { type: [Number], required: true },
+  },
+  categorie: String,
+  nom: String,
+  adresse: String,
+  com_name: String,
+  code_postal: Number,
+  description: String,
+  familles_des_produits: [String],
+  familles_des_produits_restreintes: [String],
+});
+
+const Producer = mongoose.model('Producer', producerSchema);
+
+module.exports = Producer;
--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const producerRoutes = require('./routes/producer.routes');
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+
+app.use('/api/import-csv', producerRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Serveur lancé sur le port ${PORT}`));
