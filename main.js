45affafb186601b8b2ed0410fc174c45b4ab7c6d
Claire Fernandes <104427590+clairef2310@users.noreply.github.com>
2025-08-30 19:06:45 +0200

    feat(sentry): add sentry (#24)

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+require('./config/sentry.js');
 const express = require('express');
+const Sentry = require('@sentry/node');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const producerRoutes = require('./routes/producer.routes');
@@ -10,7 +12,15 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+app.use(Sentry.Handlers.requestHandler());
+
 app.use('/api/import-csv', importDataRoutes);
 app.use('/api/producers', producerRoutes);
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use((err, req, res) => {
+  res.status(500).json({ error: 'Something went wrong!' });
+});
 
 module.exports = app;
