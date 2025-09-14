3c0c7153c6bdd103119414eee57df81357a6cc0a
Claire Fernandes <104427590+clairef2310@users.noreply.github.com>
2025-09-14 13:12:33 +0200

    fix(CORS): fix authorization (#32)

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://api.ptit-moulin.fr'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   }),
